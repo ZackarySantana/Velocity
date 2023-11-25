3976ecfac6a5b0103e4afe48cd71234f9f18cd13
ZackarySantana <zackzackyack@gmail.com>
2023-11-25 12:51:44 -0500

    Remove extra main function

--- cmd/workflows/main.go
+++ cmd/workflows/main.go
@@ -2,32 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/zackarysantana/velocity/src/config"
 )
 
-var data = `
-a: Easy!
-b:
-  c: 2
-  d: [3, 4]
-`
-
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: filename")
-		return
-	}
-	filename := os.Args[1]
-	config, err := config.ReadConfigFromFile(filename)
-	if err != nil {
-		panic(err)
-	}
-
-	// Loop through tests
-	for testName, test := range config.Tests {
-		fmt.Println(testName)
-		fmt.Println(test)
-	}
+	fmt.Println("Hello, playground")
 }
