1ebdd7246a648c2f1505b01641ba72774ff61ab4
ZackarySantana <zackzackyack@gmail.com>
2023-11-29 21:05:03 -0500

    Add more of the client

--- src/clients/velocityv1.go
+++ src/clients/velocityv1.go
@@ -2,18 +2,30 @@ package clients
 
 import (
 	"errors"
+	"net/http"
 	"os"
+	"strings"
 
+	"github.com/zackarysantana/velocity/internal/db"
 	"github.com/zackarysantana/velocity/src/config"
 )
 
 type VelocityClientV1 struct {
 	BaseURL string
+
+	SessionToken *string
 }
 
 func NewVelocityClientV1(baseURL string) *VelocityClientV1 {
 	return &VelocityClientV1{
-		BaseURL: baseURL,
+		BaseURL: strings.TrimSuffix(baseURL, "/"),
+	}
+}
+
+func NewVelocityClientV1WithSessionToken(baseURL, sessionToken string) *VelocityClientV1 {
+	return &VelocityClientV1{
+		BaseURL:      strings.TrimSuffix(baseURL, "/"),
+		SessionToken: &sessionToken,
 	}
 }
 
@@ -23,9 +35,7 @@ func NewVelocityClientV1FromEnv() (*VelocityClientV1, error) {
 		return nil, errors.New("VELOCITY_SERVER is not set")
 	}
 
-	return &VelocityClientV1{
-		BaseURL: baseURL,
-	}, nil
+	return NewVelocityClientV1(baseURL), nil
 }
 
 func NewVelocityClientV1FromConfig(c config.Config) (*VelocityClientV1, error) {
@@ -34,4 +44,19 @@ func NewVelocityClientV1FromConfig(c config.Config) (*VelocityClientV1, error) {
 	}
 
 	return NewVelocityClientV1(*c.Config.Server), nil
+}
+
+func (v *VelocityClientV1) Login(username, password string) (*db.User, error) {
+	response, err := http.Get(v.makeRoute("/login"))
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	// Parse body
+	return nil, nil
+}
+
+func (v *VelocityClientV1) makeRoute(route string) string {
+	return v.BaseURL + "/api/v1/" + strings.TrimPrefix(route, "/")
 }
