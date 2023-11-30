b324fb55078885f88d029c549c5453088b676011
ZackarySantana <zackzackyack@gmail.com>
2023-11-29 21:43:32 -0500

    Create upcreate to websocket function

---
+++ internal/api/api.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var UpgradeToWebsocket = upgrader.Upgrade
