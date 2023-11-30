3022d35d384151b97442e34e09abd6765a58b7d4
ZackarySantana <zackzackyack@gmail.com>
2023-11-29 21:45:18 -0500

    Embed middleware in groups

--- internal/api/v1/v1.go
+++ internal/api/v1/v1.go
@@ -9,11 +9,9 @@ import (
 func CreateV1App(client db.Connection) (*gin.Engine, error) {
 	router := gin.Default()
 
-	v1 := router.Group("/v1")
-	v1.Use((middleware.UseDB(client)))
+	v1 := router.Group("/v1", middleware.UseDB(client))
 
-	authorizedV1 := v1.Group("/")
-	authorizedV1.Use(middleware.Auth)
+	authorizedV1 := v1.Group("/", middleware.Auth)
 	authorizedV1.GET("/jobs", middleware.QueryAmount(1), GetJobs)
 	authorizedV1.GET("/jobs/dequeue", middleware.QueryAmount(1), GetJobs)
 
