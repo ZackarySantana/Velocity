d32f8ec4813b480c975a6aa03a76763dda7a0f9c
Zackary Santana <zackzackyack@gmail.com>
2024-01-13 23:25:02 -0500

    feat: add yaml struct tags to config

--- src/config/raw.go
+++ src/config/raw.go
@@ -7,95 +7,95 @@ import (
 type RawEnv []string
 
 type RawCommand struct {
-	WorkingDirectory *string
-	Env              *RawEnv
+	WorkingDirectory *string `yaml:"working_dir"`
+	Env              *RawEnv `yaml:"env"`
 
 	// Prebuilt
-	Prebuilt *string
-	Params   *[]map[string]string
+	Prebuilt *string              `yaml:"prebuilt"`
+	Params   *[]map[string]string `yaml:"params"`
 
 	// Command
-	Command *string
+	Command *string `yaml:"command"`
 
 	// Operations
-	Operation *string
+	Operation *string `yaml:"operation"`
 }
 
 type RawTest struct {
-	Name     string
-	Commands []RawCommand
+	Name     string       `yaml:"name"`
+	Commands []RawCommand `yaml:"commands"`
 
-	WorkingDirectory *string
-	Env              *RawEnv
+	WorkingDirectory *string `yaml:"working_dir"`
+	Env              *RawEnv `yaml:"env"`
 }
 
 type RawTestSection []RawTest
 
 type RawRuntime struct {
-	Name string
+	Name string `yaml:"name"`
 
-	Env *RawEnv
+	Env *RawEnv `yaml:"env"`
 
 	// Docker runtime
-	Image *string
+	Image *string `yaml:"image"`
 
 	// Bare metal
-	Machine *string
+	Machine *string `yaml:"machine"`
 }
 
 type RawRuntimeSection []RawRuntime
 
 type RawBuild struct {
-	Name         string
-	BuildRuntime string
-	Output       string
-	Commands     []RawCommand
+	Name         string       `yaml:"name"`
+	BuildRuntime string       `yaml:"build_runtime"`
+	Output       string       `yaml:"output"`
+	Commands     []RawCommand `yaml:"commands"`
 
-	OutputRuntime *string
-	OutputCmd     *string
+	OutputRuntime *string `yaml:"output_runtime"`
+	OutputCmd     *string `yaml:"output_cmd"`
 }
 
 type RawBuildSection []RawBuild
 
 type RawDeployment struct {
-	Name     string
-	Commands []RawCommand
+	Name     string       `yaml:"name"`
+	Commands []RawCommand `yaml:"commands"`
 
-	Workflows []string
+	Workflows []string `yaml:"workflows"`
 }
 
 type RawDeploymentSection []RawDeployment
 
 type RawWorkflowGroup struct {
-	Name string
+	Name string `yaml:"name"`
 
-	Runtimes []string
+	Runtimes []string `yaml:"runtimes"`
 
-	Tests []string
+	Tests []string `yaml:"tests"`
 }
 
 type RawWorkflow struct {
-	Name string
+	Name string `yaml:"name"`
 
-	Groups []RawWorkflowGroup
+	Groups []RawWorkflowGroup `yaml:"groups"`
 }
 
 type RawWorkflowSection []RawWorkflow
 
 type RawConfigSection struct {
-	Project string
+	Project string `yaml:"project"`
 
-	Server *string
-	UI     *string
+	Server *string `yaml:"server"`
+	UI     *string `yaml:"ui"`
 }
 
 type RawConfiguration struct {
-	TestSection       RawTestSection
-	RuntimeSection    RawRuntimeSection
-	BuildSection      RawBuildSection
-	DeploymentSection RawDeploymentSection
-	WorkflowSection   RawWorkflowSection
-	ConfigSection     RawConfigSection
+	TestSection       RawTestSection       `yaml:"tests"`
+	RuntimeSection    RawRuntimeSection    `yaml:"runtimes"`
+	BuildSection      RawBuildSection      `yaml:"builds"`
+	DeploymentSection RawDeploymentSection `yaml:"deployments"`
+	WorkflowSection   RawWorkflowSection   `yaml:"workflows"`
+	ConfigSection     RawConfigSection     `yaml:"config"`
 }
 
 func Parse(data []byte) (*RawConfiguration, error) {
