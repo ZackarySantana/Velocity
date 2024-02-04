4289880622bf45a34db1c5cd4f5a27a4e283ebc4
ZackarySantana <zackzackyack@gmail.com>
2024-02-03 21:57:01 -0500

    feat: create basic api with connection to mongo

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zackarysantana/velocity/internal/api/middleware"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
-	fmt.Println("TODO: API main")
+	db, err := getEnv("MONGODB_DATABASE")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	username, err := getEnv("MONGODB_USERNAME")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	password, err := getEnv("MONGODB_PASSWORD")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	uri, err := getEnv("MONGODB_URI")
+	if err != nil {
+		log.Fatal(err)
+	}
+	path := fmt.Sprintf(uri, username, password)
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(path).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(context.Background(), opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	engine := gin.Default()
+
+	agent := engine.Group("/agent")
+	agent.Use(middleware.AuthWithMongoDBAndUsernameAndPasswordFromBody(*client, db, "users"))
+	agent.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	engine.Run(":8080")
+}
+
+func getEnv(name string) (string, error) {
+	value, exists := os.LookupEnv(name)
+	if !exists {
+		return "", fmt.Errorf("%s not set", name)
+	}
+	return value, nil
 }
