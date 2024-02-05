b755c082432cc6eb6608ab30e12e5d995f1bc549
ZackarySantana <zackzackyack@gmail.com>
2024-02-04 23:30:59 -0500

    refactor: add some docs

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -9,11 +9,15 @@ import (
 	"github.com/zackarysantana/velocity/internal/cli/flags"
 )
 
+// app wraps the cli runtime to centralize common
+// use-cases like error handling and logging.
 type app struct {
 	exitOnError bool
 	silent      bool
 }
 
+// createCliApp creates the cli application with the
+// given app instance.
 func createCliApp(app *app) *cli.App {
 	return &cli.App{
 		Name:    "velocity",
@@ -30,11 +34,14 @@ func createCliApp(app *app) *cli.App {
 	}
 }
 
+// Run creates and starts the cli application.
 func Run() error {
-	app := app{}
-	return createCliApp(&app).Run(os.Args)
+	return createCliApp(&app{}).Run(os.Args)
 }
 
+// RunAndExitOnError creates and starts the cli application
+// and if an error is encountered, it will log the error and
+// exit with a non-zero exit code.
 func RunAndExitOnError() {
 	app := app{
 		exitOnError: true,
@@ -43,32 +50,46 @@ func RunAndExitOnError() {
 	err := createCliApp(&app).Run(os.Args)
 
 	if err != nil {
-		log.Fatal("It seems there was an error: ", err)
+		if !app.silent {
+			log.Fatal("It seems there was an error: ", err)
+		}
 		cli.OsExiter(1)
 	}
 }
 
+// exitHandler is an interface that allows for custom
+// error handling. If a cli command returns an error that
+// implements this, the handle function will be called. If
+// the handle function returns true, the default logging
+// will not take place. If it is false, the default logging
+// will take place.
 type exitHandler interface {
-	handle(app, error)
+	handle(app, error) bool
 }
 
+// exitErrHandler is the handles the exit error for the cli.
+// If checks if the error implements exitHandler and calls
+// the handle function if it does.
 func (a *app) exitErrHandler(c *cli.Context, err error) {
 	if err == nil {
 		return
 	}
-	switch handler := err.(type) {
-	case exitHandler:
-		handler.handle(*a, err)
-	default:
-		if !a.isSilent(c) {
-			fmt.Println(err)
+	if handler, ok := err.(exitHandler); ok {
+		if handler.handle(*a, err) {
+			return
 		}
-		if a.exitOnError {
-			cli.OsExiter(1)
-		}
+	}
+	if !a.isSilent(c) {
+		fmt.Println(err)
+	}
+	if a.exitOnError {
+		cli.OsExiter(1)
 	}
 }
 
+// isSilent checks if the silent field has been set to true,
+// and if not, it checks the cli context for the exit code
+// only flag.
 func (a *app) isSilent(c *cli.Context) bool {
 	if a.silent {
 		return true
