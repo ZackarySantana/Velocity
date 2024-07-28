72d6024932ce0707a91172e6bb277dd41101eae9
ZackarySantana <zackzackyack@gmail.com>
2024-07-28 14:41:01 -0400

    refactor: take out logic for logger from main command logic

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -3,13 +3,8 @@ package cmd
 import (
 	"context"
 	"log/slog"
-	"os"
 
-	"github.com/samber/oops"
-	slogmulti "github.com/samber/slog-multi"
 	"github.com/urfave/cli/v3"
-	"github.com/zackarysantana/velocity/internal/stats"
-	"github.com/zackarysantana/velocity/internal/vlog"
 )
 
 func CreateCommand() *cli.Command {
@@ -20,49 +15,13 @@ func CreateCommand() *cli.Command {
 		EnableShellCompletion: true,
 		Metadata:              map[string]interface{}{},
 		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:  "mode",
-				Usage: "set the mode to `debug|quiet`",
-				Validator: func(s string) error {
-					if s == "" {
-						return nil
-					}
-					if s != "debug" && s != "quiet" {
-						return oops.In("flags").Errorf("mode must be debug or quiet")
-					}
-					return nil
-				},
-			},
-			&cli.BoolFlag{
-				Name:  "verbose",
-				Usage: "enable verbose output",
-			},
+			loggerModeFlag,
+			verboseFlag,
 		},
 		Before: func(ctx context.Context, cmd *cli.Command) error {
-			// TODO: Custom handlers for different providers
-			// one that is just plain text for each message.
-			level := slog.LevelInfo
-			switch cmd.String("mode") {
-			case "debug":
-				level = slog.LevelDebug
-			case "error":
-				level = slog.LevelError
-			case "quiet":
-				level = 12
+			if err := setLogger(ctx, cmd); err != nil {
+				return err
 			}
-			var stdLogger slog.Handler
-			if cmd.Bool("verbose") {
-				stdLogger = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: level})
-			} else {
-				stdLogger = vlog.NewPlainHandler(os.Stdout, &vlog.Options{Level: level})
-			}
-
-			logger := slog.New(slogmulti.Fanout(
-				stdLogger,
-			))
-			cmd.Metadata["logger"] = logger
-
-			logger.Debug("Starting logger", "cmd", os.Args, "version", cmd.Version, "ip", stats.GetIP())
 			return nil
 		},
 		Commands: []*cli.Command{
---
+++ internal/cmd/logger.go
@@ -0,0 +1,102 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/samber/oops"
+	slogmulti "github.com/samber/slog-multi"
+	"github.com/urfave/cli/v3"
+	"github.com/zackarysantana/velocity/internal/stats"
+	"github.com/zackarysantana/velocity/internal/vlog"
+)
+
+type LoggerMode int
+
+const (
+	Debug LoggerMode = iota
+	Quiet
+	maxLoggerMode
+)
+
+func (m LoggerMode) String() string {
+	return loggerModes[m]
+}
+
+func (m LoggerMode) Valid() bool {
+	return 0 <= m && m < maxLoggerMode
+}
+
+func getLoggerMode(mode string) *LoggerMode {
+	for k, v := range loggerModes {
+		if v == mode {
+			return &k
+		}
+	}
+	return nil
+}
+
+func getAllLoggerModes() []string {
+	modes := make([]string, 0, len(loggerModes))
+	for _, v := range loggerModes {
+		modes = append(modes, v)
+	}
+	return modes
+}
+
+var (
+	loggerModes = map[LoggerMode]string{
+		Debug: "debug",
+		Quiet: "quiet",
+	}
+	loggerModeFlag = &cli.StringFlag{
+		Name:  "mode",
+		Usage: fmt.Sprintf("set the mode to `%s`", strings.Join(getAllLoggerModes(), "|")),
+		Validator: func(s string) error {
+			if s == "" {
+				return nil
+			}
+			if getLoggerMode(s) == nil {
+				return oops.In("flags").Errorf("mode must be one of: %s", strings.Join(getAllLoggerModes(), ", "))
+			}
+			return nil
+		},
+	}
+	verboseFlag = &cli.BoolFlag{
+		Name:  "verbose",
+		Usage: "enable verbose output",
+	}
+)
+
+func setLogger(_ context.Context, cmd *cli.Command) error {
+	level := slog.LevelInfo
+	switch cmd.String("mode") {
+	case "":
+		level = slog.LevelInfo
+	case "debug":
+		level = slog.LevelDebug
+	case "error":
+		level = slog.LevelError
+	case "quiet":
+		level = 12
+	default:
+		return oops.In("flags").Errorf("mode must be one of: %s", strings.Join(getAllLoggerModes(), ", "))
+	}
+	var stdLogger slog.Handler
+	if cmd.Bool("verbose") {
+		stdLogger = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: level})
+	} else {
+		stdLogger = vlog.NewPlainHandler(os.Stdout, &vlog.Options{Level: level})
+	}
+
+	logger := slog.New(slogmulti.Fanout(
+		stdLogger,
+	))
+	cmd.Metadata["logger"] = logger
+
+	logger.Debug("Starting logger", "cmd", os.Args, "version", cmd.Version, "ip", stats.GetIP())
+	return nil
+}
