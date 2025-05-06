dea0d4edaa79c0d4db45df18583fd73e5c25ec15
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-05-05 22:44:21 -0400

    feat: priority queue test should use erroris

--- internal/service/servicetest/priority_queue.go
+++ internal/service/servicetest/priority_queue.go
@@ -22,7 +22,7 @@ func TestPriorityQueue(t *testing.T, pqGen func() service.PriorityQueue[any, str
 			assert.Equal(t, "test", item.Payload)
 
 			item, err = pq.Pop(ctx, "test")
-			assert.Equal(t, service.ErrEmptyQueue, err)
+			assert.ErrorIs(t, service.ErrEmptyQueue, err)
 		},
 		"MultiItem": func(t *testing.T, pq service.PriorityQueue[any, string]) {
 			err := pq.Push(ctx, "test", service.PriorityQueueItem[string]{Priority: 1, Payload: "test1"}, service.PriorityQueueItem[string]{Priority: 2, Payload: "test2"})
@@ -37,7 +37,7 @@ func TestPriorityQueue(t *testing.T, pqGen func() service.PriorityQueue[any, str
 			require.Equal(t, "test1", item.Payload)
 
 			item, err = pq.Pop(ctx, "test")
-			assert.Equal(t, service.ErrEmptyQueue, err)
+			assert.ErrorIs(t, service.ErrEmptyQueue, err)
 		},
 		"MutliQueue": func(t *testing.T, pq service.PriorityQueue[any, string]) {
 			err := pq.Push(ctx, "test1", service.PriorityQueueItem[string]{Priority: 1, Payload: "test1"})
@@ -51,14 +51,14 @@ func TestPriorityQueue(t *testing.T, pqGen func() service.PriorityQueue[any, str
 			require.Equal(t, "test2", item.Payload)
 
 			item, err = pq.Pop(ctx, "test2")
-			assert.Equal(t, service.ErrEmptyQueue, err)
+			assert.ErrorIs(t, service.ErrEmptyQueue, err)
 
 			item, err = pq.Pop(ctx, "test1")
 			require.NoError(t, err)
 			require.Equal(t, "test1", item.Payload)
 
 			item, err = pq.Pop(ctx, "test1")
-			assert.Equal(t, service.ErrEmptyQueue, err)
+			assert.ErrorIs(t, service.ErrEmptyQueue, err)
 		},
 		"UnfinishedItems": func(t *testing.T, pq service.PriorityQueue[any, string]) {
 			err := pq.Push(ctx, "test", service.PriorityQueueItem[string]{Priority: 1, Payload: "test1"}, service.PriorityQueueItem[string]{Priority: 2, Payload: "test2"})
