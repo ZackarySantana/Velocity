da16ffe17d59b18c18908f76185b6d6cdc08d39b
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-05-05 22:45:25 -0400

    fix: clean up priority queue test in mock

--- internal/service/mock/priority_queue_test.go
+++ internal/service/mock/priority_queue_test.go
@@ -8,9 +8,7 @@ import (
 )
 
 func TestMongoPriorityQueue(t *testing.T) {
-	i := 0
 	pqGen := func() service.PriorityQueue[any, string] {
-		i++
 		return NewPriorityQueue[any, string](NewIDCreator[any]())
 	}
 
